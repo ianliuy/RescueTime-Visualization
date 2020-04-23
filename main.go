7106e8980d15b26f50cc24c0c94fd91f6d333ffb
sakila <i@yiyangliu.me>
2020-04-23 12:35:38 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,34 +9,34 @@ import (
 
 func main() {
 	//Variables:
-	//  rtapi: string, Your RescueTime API Key,
+	// rtapi: string, Your RescueTime API Key,
 	//		https://www.rescuetime.com/anapi/manage
 	//	un: string, Username of your Github account
 	//	pw: string, Password of your Github account
-	//  token:, string, "personal access token" of your Github account:
+	// token:, string, "personal access token" of your Github account:
 	//		https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line
 	//	repo: string, "HTTPS URLs" of your repository
 	//		https://help.github.com/en/github/using-git/which-remote-url-should-i-use#cloning-with-https-urls-recommended
 	//	dir: string, Directory path that your repository cloned into
 	//	fpath: string, Path of "README.md" file of your repository
-	//  auth: http.BasicAuth, the "auth" Type contains your Github username & password
+	// auth: http.BasicAuth, the "auth" Type contains your Github username & password
 	//		or username & your "personal access token"
-	//  nrt, rescuetime.RescueTime, basic RescueTime object
-	//  data, rescuetime.AnalyticData, a json-like object contains your today's detailed data
-	//  today, []string, transformed by "data" to a slice of string
-	//  				 that printed like a markdown table,
-	//  	example:
+	// nrt, rescuetime.RescueTime, basic RescueTime object
+	// data, rescuetime.AnalyticData, a json-like object contains your today's detailed data
+	// today, []string, transformed by "data" to a slice of string
+	// 				 that printed like a markdown table,
+	// 	example:
 	//		[[|Rank|Activity|Time|Category|Label|],
 	//		 [|-|-|-|-|-|],
 	//		 [|1|goland64|4h37m|Dev|2|],
 	//		 [|2|github.com|1h14m|Dev|2|],
 	//		 ...
 	//		 [|15|dllhost|4m30s|Utils|1|]]
-	//  history: []string, read old "README.md" by lines
-	//  hd: string, the latest date of your old "README.md", like "2020-04-21"
-	//  td: string, today's date, like "2020-04-22"
+	// history: []string, read old "README.md" by lines
+	// hd: string, the latest date of your old "README.md", like "2020-04-21"
+	// td: string, today's date, like "2020-04-22"
 	//		the "README.md" file will update only when hd < td
-	//  cont: []string, the new content of "README.md", mixed by "today" and "history"
+	// cont: []string, the new content of "README.md", mixed by "today" and "history"
 
 	err := os.Setenv("http_proxy", "http://127.0.0.1:1080")
 	if err != nil {}
@@ -83,7 +83,7 @@ func main() {
 
 	hd := history[3][14:24]
 	td := time.Now().Format("2006-01-02")
-	if td < hd {fmt.Println("Today is the latest day, nothing need to update")
+	if td <= hd {fmt.Println("Today is the latest day, nothing need to update")
 	} else {
 		cont := getContent(today, history)
 		//for _, row := range cont { fmt.Println(row)}
